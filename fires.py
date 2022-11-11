5d1dd8d6aed6a305ee119956f6da8adf49adcc72
Azaya89 <akoritaisaiah@gmail.com>
2022-11-11 18:20:56 +0100

    commented the code

--- fires.py
+++ fires.py
@@ -4,40 +4,44 @@ from plotly import offline
 from datetime import datetime
 
 
-filename = 'global_fires_7d.csv'
+filename = "global_fires_7d.csv"
 
+# Open file in python csv-readable format
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
+    ## Print header rows
     # for i,r in enumerate(header_row):
     #     print(i,r)
 
-    lats, lons, brights, dates = [],[],[],[]
+    # Get details for the plot
+    lats, lons, brights, dates = [], [], [], []
     for row in reader:
-        date = datetime.strptime(row[5], '%Y-%m-%d')
+        date = datetime.strptime(row[5], "%Y-%m-%d")
         lats.append(row[0])
         lons.append(row[1])
         brights.append(float(row[2]))
         dates.append(date)
 
+# Design and plot data
 data = [
     {
-        'type': 'scattergeo',
-        'lat': lats,
-        'lon': lons,
-        'text': dates,
-        'marker': {
-            'size': [bright/20 for bright in brights],
-            'color': brights,
-            'colorscale': 'Earth',
-            'reversescale': True,
-            'colorbar': {'title': 'Intensity'},
-            },
+        "type": "scattergeo",
+        "lat": lats,
+        "lon": lons,
+        "text": dates,
+        "marker": {
+            "size": [bright / 20 for bright in brights],
+            "color": brights,
+            "colorscale": "Viridis",
+            "reversescale": True,
+            "colorbar": {"title": "Intensity"},
+        },
     }
 ]
 
 my_layout = Layout(title=f"Fires around the world from {dates[0]} to {dates[-1]}")
 
-fig = {'data': data, 'layout': my_layout}
+fig = {"data": data, "layout": my_layout}
 offline.plot(fig)
